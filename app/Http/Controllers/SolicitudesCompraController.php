4a14933db08bda77a459d680d4d7f18b5db90857
JulianLargo72 <julianbetancur104@gmail.com>
2024-09-11 13:51:51 -0500

    incorporando a la vista de solicitudes de compras los cambios para asegurar que solo se muestren los centros de costos relevantes para el área del usuario autenticado.

--- app/Http/Controllers/SolicitudesCompraController.php
+++ app/Http/Controllers/SolicitudesCompraController.php
@@ -80,8 +80,18 @@ class SolicitudesCompraController extends Controller
         $solicitudesCompra = new SolicitudesCompra();
         $solicitudesCompra->prefijo = $this->generatePrefix();
         $users = User::all();
-        $centrosCostos = CentrosCosto::all();
         $fechaActual = Carbon::now()->toDateString();
+    
+        // Obtener el área del usuario autenticado
+        $user = auth()->user();
+        $areaId = $user->id_area;
+    
+        // Obtener los centros de costos asociados a las clasificaciones de centros del área del usuario
+        $centrosCostos = CentrosCosto::whereIn('id_clasificaciones_centros', function ($query) use ($areaId) {
+            $query->select('id_clasificaciones_centros')
+                  ->from('clasificaciones_centros_areas')
+                  ->where('id_areas', $areaId);
+        })->get();
     
         // Mapeo de permisos a los nombres de los niveles uno
         $permissions = [
