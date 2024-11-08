9f498cd91c909cf2c0b797b898c6fe082135c0ae
JulianLargo72 <julianbetancur104@gmail.com>
2024-11-08 14:36:48 -0500

    Este enfoque de diseño facilita la navegación y permite al usuario ver la información de cada clasificación de manera rápida y organizada con su centro de costo relacionado. Tambien mostrará las áreas relacionadas en una sola línea, haciendo la vista más compacta y fácil de leer.

--- resources/views/clasificaciones-centro/index.blade.php
+++ resources/views/clasificaciones-centro/index.blade.php
@@ -2,6 +2,15 @@
 
 @section('title', 'Clasificaciones Centro')
 
+@section('css')
+<style>
+    .scrollable-list {
+        max-height: 300px;
+        overflow-y: auto;
+    }
+</style>
+@endsection
+
 @section('content')
 <br>
     <div class="container-fluid">
@@ -10,15 +19,9 @@
                 <div class="card">
                     <div class="card-header">
                         <div style="display: flex; justify-content: space-between; align-items: center;">
-
                             <span id="card_title">
                                 {{ __('Clasificaciones Centros') }}
                             </span>
-                            <div class="float-right d-flex align-items-center">
-                                <a href="{{ route('clasificaciones-centros.create') }}" class="btn btn-primary btn-sm" data-placement="left">
-                                    {{ __('Crear Nuevo') }}
-                                </a>
-                            </div>
                         </div>
                     </div>
                     @if ($message = Session::get('success'))
@@ -26,37 +29,45 @@
                     @endif
 
                     <div class="card-body bg-white">
-                        <div class="table-responsive">
-                            <table class="table table-striped table-hover datatable">
-                                <thead class="thead">
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nombre</th>
-                                        <th>Áreas</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
+                        <div class="row">
+                            <!-- Columna Clasificacion Centro Costo -->
+                            <div class="col-md-6">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <h5>Clasificaciones Centros</h5>
+                                    <a href="{{ route('clasificaciones-centros.create') }}" class="btn btn-primary btn-sm">{{ __('Crear Nuevo') }}</a>
+                                </div>
+                                <div class="list-group scrollable-list" id="ClasificacionesCentrosList">
                                     @foreach ($clasificacionesCentros as $clasificacionesCentro)
-                                        <tr>
-                                            <td>{{ ++$i }}</td>
-                                            <td>{{ $clasificacionesCentro->nombre }}</td>
-                                            <td>
+                                        <a href="#" class="list-group-item list-group-item-action" data-id="{{ $clasificacionesCentro->id }}">
+                                            <strong>{{ $clasificacionesCentro->nombre }}</strong>
+                                            <p class="card-text"><strong>Áreas:</strong> 
                                                 {{ $clasificacionesCentro->areas->pluck('nombre')->implode(', ') }}
-                                            </td>
-                                            <td>
-                                                <form action="{{ route('clasificaciones-centros.destroy', $clasificacionesCentro->id) }}" class="delete-form" method="POST">
-                                                    <a class="btn btn-sm btn-primary" href="{{ route('clasificaciones-centros.show', $clasificacionesCentro->id) }}"><i class="fa fa-fw fa-eye"></i></a>
-                                                    <a class="btn btn-sm btn-success" href="{{ route('clasificaciones-centros.edit', $clasificacionesCentro->id) }}"><i class="fa fa-fw fa-edit"></i></a>
-                                                    @csrf
-                                                    @method('DELETE')
-                                                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-fw fa-trash"></i></button>
-                                                </form>
-                                            </td>
-                                        </tr>
+                                            </p>
+                                            <a href="{{ route('clasificaciones-centros.edit', $clasificacionesCentro->id) }}" class="btn btn-warning btn-sm float-right">{{ __('Editar') }}</a>
+                                        </a>
                                     @endforeach
-                                </tbody>
-                            </table>                            
+                                </div>
+                            </div>
+    
+                            <!-- Columna Centro Costo -->
+                            <div class="col-md-6">
+                                <div class="d-flex justify-content-between align-items-center mb-3">
+                                    <h5>Centros Costos</h5>
+                                    <a href="{{ route('centros-costos.create') }}" class="btn btn-primary btn-sm">{{ __('Crear Nuevo') }}</a>
+                                </div>
+                                <form action="{{ route('centros-costos.import') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+                                    <div class="input-group">
+                                        <input type="file" class="form-control @error('file') is-invalid @enderror" name="file" aria-label="Upload" required>
+                                        <button class="btn btn-secondary" type="submit">Importar</button>
+                                        {!! $errors->first('file', '<div class="invalid-feedback" role="alert"><strong>:message</strong></div>') !!}
+                                    </div>
+                                    <br>
+                                </form>
+                                <div class="list-group scrollable-list" id="centrosCostosList">
+                                    <div class="alert alert-info">No se ha seleccionado ninguna clasificación centro aún.</div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -64,3 +75,7 @@
         </div>
     </div>
 @endsection
+
+@push('js')
+    <script src="{{ asset('js/clasificaciones-centro/cargarCentroCosto.js') }}"></script>
+@endpush
