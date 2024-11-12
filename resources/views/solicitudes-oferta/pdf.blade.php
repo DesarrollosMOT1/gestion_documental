970028ccb110b645f038f4df49387d4040ceaafc
JulianLargo72 <julianbetancur104@gmail.com>
2024-11-12 11:38:38 -0500

    corrigiendo error en el pdf generado en la solicitud de oferta

--- resources/views/solicitudes-oferta/pdf.blade.php
+++ resources/views/solicitudes-oferta/pdf.blade.php
@@ -48,8 +48,8 @@
                     <tr>
                         <td>{{ ++$i }}</td>
                         <td>{{ $consolidacion->solicitudesElemento->nivelesTres->nombre ?? 'N/A' }}</td>
-                        <td>{{ $consolidacion->cantidad }}</td>
                         <td>{{ $consolidacion->consolidacione->cantidad ?? 'N/A' }}</td>
+                        <td>{{ $consolidacion->descripcion ?? 'N/A' }}</td>
                     </tr>
                 @endforeach
             </tbody>
