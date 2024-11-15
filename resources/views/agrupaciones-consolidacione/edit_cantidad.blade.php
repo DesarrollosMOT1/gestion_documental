824a715cbf593c4f2a694b7d3b7c77e309556425
JulianLargo72 <julianbetancur104@gmail.com>
2024-11-15 15:21:12 -0500

    agregando a la cantidad de los elementos consolidados el metodo old de laravel y mensaje en caso de error

--- resources/views/agrupaciones-consolidacione/edit_cantidad.blade.php
+++ resources/views/agrupaciones-consolidacione/edit_cantidad.blade.php
@@ -14,7 +14,13 @@
                     <tr>
                         <th scope="row">Cantidad</th>
                         <td>
-                            <input type="number" class="form-control" name="cantidad" value="{{ old('cantidad', $elementoConsolidado->solicitudesElemento->cantidad) }}">
+                            <input type="number" class="form-control @error('cantidad') is-invalid @enderror" 
+                                id="cantidad" name="cantidad" value="{{ old('cantidad', $elementoConsolidado->solicitudesElemento->cantidad) }}">
+                            @error('cantidad')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror
                         </td>
                     </tr>
                     <tr>
